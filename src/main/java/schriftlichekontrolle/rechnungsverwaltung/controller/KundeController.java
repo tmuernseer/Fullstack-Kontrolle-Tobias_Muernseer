8b30dac7ffd4d36789fff187cdd8250c75b615dd
Tobias Mürnseer <tmuernseer@tsn.at>
2020-11-18 14:30:57 +0100

    KundeRepository Autowired

--- src/main/java/schriftlichekontrolle/rechnungsverwaltung/controller/KundeController.java
+++ src/main/java/schriftlichekontrolle/rechnungsverwaltung/controller/KundeController.java
@@ -1,4 +1,12 @@
 package schriftlichekontrolle.rechnungsverwaltung.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import schriftlichekontrolle.rechnungsverwaltung.repository.KundeRepository;
+
+@RestController
 public class KundeController {
+
+    @Autowired
+    private KundeRepository kundeRepository;
 }
