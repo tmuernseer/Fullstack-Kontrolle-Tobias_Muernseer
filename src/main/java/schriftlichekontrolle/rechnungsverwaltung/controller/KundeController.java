e1cbd8aaa355da97b14eb367d289d2e5e4957c61
Tobias Mürnseer <tmuernseer@tsn.at>
2020-11-18 14:32:50 +0100

    GetMapping für Kunde erstellt

--- src/main/java/schriftlichekontrolle/rechnungsverwaltung/controller/KundeController.java
+++ src/main/java/schriftlichekontrolle/rechnungsverwaltung/controller/KundeController.java
@@ -1,7 +1,11 @@
 package schriftlichekontrolle.rechnungsverwaltung.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import schriftlichekontrolle.rechnungsverwaltung.model.Kunde;
 import schriftlichekontrolle.rechnungsverwaltung.repository.KundeRepository;
 
 @RestController
@@ -9,4 +13,10 @@ public class KundeController {
 
     @Autowired
     private KundeRepository kundeRepository;
+
+    @GetMapping("/kunden")
+    public Page<Kunde> getAllBrands(Pageable pageable) {
+        return kundeRepository.findAll(pageable);
+    }
+
 }
