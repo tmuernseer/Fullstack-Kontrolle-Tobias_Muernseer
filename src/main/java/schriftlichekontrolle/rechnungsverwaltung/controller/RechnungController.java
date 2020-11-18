5300b8556d2215c2e09e1f50d6d943b000dc3f28
Tobias Mürnseer <tmuernseer@tsn.at>
2020-11-18 14:29:22 +0100

    RechnungsRepository Autowired

--- src/main/java/schriftlichekontrolle/rechnungsverwaltung/controller/RechnungController.java
+++ src/main/java/schriftlichekontrolle/rechnungsverwaltung/controller/RechnungController.java
@@ -1,4 +1,12 @@
 package schriftlichekontrolle.rechnungsverwaltung.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import schriftlichekontrolle.rechnungsverwaltung.repository.RechnungRepository;
+
+@RestController
 public class RechnungController {
+
+    @Autowired
+    private RechnungRepository rechnungRepository;
 }
