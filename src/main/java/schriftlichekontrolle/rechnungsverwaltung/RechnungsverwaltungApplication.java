8ca45898e97d01a528e9ca1234ff194f407238a1
Tobias Mürnseer <72599179+tmuernseer@users.noreply.github.com>
2020-11-18 13:46:11 +0100

    Add files via upload
    Daten von Spring Initializr

---
+++ src/main/java/schriftlichekontrolle/rechnungsverwaltung/RechnungsverwaltungApplication.java
@@ -0,0 +1,13 @@
+package schriftlichekontrolle.rechnungsverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechnungsverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RechnungsverwaltungApplication.class, args);
+	}
+
+}
